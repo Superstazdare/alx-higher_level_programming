dad48347a6f361c981a677c9067ad79090fdfd56
Superstazdare <superstaz.dare@gmail.com>
2023-06-14 02:01:00 -0700

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) 
+{
+	console.log(line);
 }
