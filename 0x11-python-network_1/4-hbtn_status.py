6360b6b8ac9653684580b482a420ef4b8a3d1213
Superstazdare <superstaz.dare@gmail.com>
2023-06-30 05:35:28 -0700

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@ import requests
 
 
 if __name__ == "__main__":
-    req = requests.get('https://intranet.hbtn.io/status')
+    req = requests.get('https://alx-intranet.hbtn.io/status')
 
     print('Body response:')
     print('\t- type: {_type}'.format(_type=type(req.text)))
